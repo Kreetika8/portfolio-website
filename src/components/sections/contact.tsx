82ddfb754eca2814838ad80c91ad399751c4eb21
Kreetika Bhetuwal <Kritikabhetuwal8@gmail.com>
2025-07-28 15:16:48 +0545

    url updated

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
 
     try {
       // const response = await fetch('/api/contact', {
-      const response = await fetch("https://portfolio-7asdhwv0z-kreetika-s-projects.vercel.app/api/contact", {
+      const response = await fetch("/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
